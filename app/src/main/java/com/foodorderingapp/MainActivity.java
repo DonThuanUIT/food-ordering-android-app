5a770a80e0c37fc6ffcd00566deed57ab440f87f
Quý ông <le2092129@gmail.com>
2026-06-13 17:52:58 +0700

    feat: implement ViewPager2 tab state adapter and fix bottom nav overlapping content leak

--- app/src/main/java/com/foodorderingapp/MainActivity.java
+++ app/src/main/java/com/foodorderingapp/MainActivity.java
@@ -13,11 +13,13 @@ import com.foodorderingapp.ui.home.vendor.VendorStatsFragment;
 import com.foodorderingapp.ui.home.vendor.VendorMenuFragment;
 import com.foodorderingapp.ui.home.vendor.VendorSettingsFragment;
 import com.google.android.material.navigation.NavigationBarView;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNav;
+    private ViewPager2 viewPager;
     private String userRole;
 
     @Override
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNav = findViewById(R.id.bottom_navigation);
+        viewPager = findViewById(R.id.view_pager);
 
         if (findViewById(R.id.toolbar) != null) {
             setSupportActionBar(findViewById(R.id.toolbar));
@@ -44,49 +47,67 @@ public class MainActivity extends AppCompatActivity {
 
         if ("VENDOR".equals(role)) {
             bottomNav.inflateMenu(R.menu.menu_vendor);
-            loadFragment(new VendorOrdersFragment());
         } else {
             bottomNav.inflateMenu(R.menu.menu_student);
-            loadFragment(new StudentHomeFragment());
         }
+
+        viewPager.setAdapter(new MainPagerAdapter(this, role));
+        viewPager.setUserInputEnabled(false);
+        viewPager.setOffscreenPageLimit(3);
 
         bottomNav.setLabelVisibilityMode(NavigationBarView.LABEL_VISIBILITY_LABELED);
 
         bottomNav.setOnItemSelectedListener(item -> {
-            Fragment selectedFragment = null;
             int id = item.getItemId();
-
-            if (id == R.id.nav_home) {
-                selectedFragment = new StudentHomeFragment();
-            } else if (id == R.id.nav_orders) {
-                selectedFragment = new StudentOrdersFragment();
-            } else if (id == R.id.nav_history) {
-                selectedFragment = new StudentHistoryFragment();
-            } else if (id == R.id.nav_profile) {
-                selectedFragment = new StudentProfileFragment();
-            }
-
-            else if (id == R.id.nav_vendor_orders) {
-                selectedFragment = new VendorOrdersFragment();
-            } else if (id == R.id.nav_vendor_stats) {
-                selectedFragment = new VendorStatsFragment();
-            } else if (id == R.id.nav_vendor_menu) {
-                selectedFragment = new VendorMenuFragment();
-            } else if (id == R.id.nav_vendor_settings) {
-                selectedFragment = new VendorSettingsFragment();
-            }
-
-            if (selectedFragment != null) {
-                loadFragment(selectedFragment);
+            if (id == R.id.nav_home || id == R.id.nav_vendor_orders) {
+                viewPager.setCurrentItem(0, false);
+                return true;
+            } else if (id == R.id.nav_orders || id == R.id.nav_vendor_stats) {
+                viewPager.setCurrentItem(1, false);
+                return true;
+            } else if (id == R.id.nav_history || id == R.id.nav_vendor_menu) {
+                viewPager.setCurrentItem(2, false);
+                return true;
+            } else if (id == R.id.nav_profile || id == R.id.nav_vendor_settings) {
+                viewPager.setCurrentItem(3, false);
                 return true;
             }
             return false;
         });
     }
 
-    private void loadFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, fragment)
-                .commit();
+    private static class MainPagerAdapter extends androidx.viewpager2.adapter.FragmentStateAdapter {
+        private final String role;
+
+        public MainPagerAdapter(@NonNull androidx.fragment.app.FragmentActivity fragmentActivity, String role) {
+            super(fragmentActivity);
+            this.role = role;
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            if ("VENDOR".equals(role)) {
+                switch (position) {
+                    case 0: return new VendorOrdersFragment();
+                    case 1: return new VendorStatsFragment();
+                    case 2: return new VendorMenuFragment();
+                    case 3: return new VendorSettingsFragment();
+                }
+            } else {
+                switch (position) {
+                    case 0: return new StudentHomeFragment();
+                    case 1: return new StudentOrdersFragment();
+                    case 2: return new StudentHistoryFragment();
+                    case 3: return new StudentProfileFragment();
+                }
+            }
+            return new Fragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return 4;
+        }
     }
 }
