6acdfb3507573b7313043968d0a4935504ee1dba
Quý ông <le2092129@gmail.com>
2026-06-23 15:04:41 +0700

    config: update BASE_URL to local server IP

--- app/src/main/java/com/foodorderingapp/utils/constants/AppConstants.java
+++ app/src/main/java/com/foodorderingapp/utils/constants/AppConstants.java
@@ -1,5 +1,5 @@
 package com.foodorderingapp.utils.constants;
 
 public class AppConstants {
-    public static final String BASE_URL = "http://192.168.1.102:8080/api/";
+    public static final String BASE_URL = "http://192.168.1.106:8080/api/";
 }
