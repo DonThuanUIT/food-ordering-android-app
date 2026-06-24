8e9d7db54edc9cb99a82787b36096a8ddce1a442
Van Tin <tindragonmanilat@gmail.com>
2026-06-24 23:27:10 +0700

    feat(message): Add realtime chat websocket support

--- app/src/main/java/com/foodorderingapp/utils/constants/AppConstants.java
+++ app/src/main/java/com/foodorderingapp/utils/constants/AppConstants.java
@@ -5,4 +5,8 @@ public class AppConstants {
     // 2. Dùng IP máy tính (VD: http://192.168.1.x:8080/api/) nếu bạn dùng điện thoại thật
     // Lưu ý: Tuyệt đối không dùng 127.0.0.1 hoặc localhost
     public static final String BASE_URL = "http://10.0.2.2:8080/api/";
+
+    public static String getWsChatUrl() {
+        return BASE_URL.replaceFirst("^http", "ws") + "ws-chat";
+    }
 }
